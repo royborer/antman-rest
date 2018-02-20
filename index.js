ccb29b356a8cfbdf2029c1001e631d77057c43a8
royborer <royb@tikalk.com>
2018-02-20 13:13:25 +0200

    Merge branch 'master' of https://github.com/royborer/antman-rest
    # Conflicts:
    #	index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 const AWS = require('aws-sdk');
-const getOneQuestion = require('getOneQuestion.js');
-const getAllQuestions = require('getAllQuestions');
-const putAnswer = require('putAnswer');
-const getResults = require('getResults');
-const getAllResults = require('getAllResults');
+const getOneQuestion = require('./getOneQuestion');
+const getAllQuestions = require('./getAllQuestions');
+const putAnswer = require('./putAnswer');
+const getResults = require('./getResults');
+const getAllResults = require('./getAllResults');
 
 const USERS_TABLE = process.env.USERS_TABLE;
 const USERS_ANSWERS_TABLE = process.env.USER_ANSWERS_TABLE;
@@ -87,5 +87,36 @@ app.post('/users', function (req, res) {
     res.json({ userId, name, options });
   });
 })
+
+
+app.post("/users/answers", function (req, res) {
+    const { userId, questionId, answer } = req.body;
+    if (typeof userId !== 'string') {
+        res.status(400).json({ error: '"userId" must be a string' });
+    } else if (typeof questionId !== 'string') {
+        res.status(400).json({ error: '"name" must be a string' });
+    }
+    else if (typeof answer !== 'string') {
+        res.status(400).json({ error: '"name" must be a string' });
+    }
+
+    const params = {
+        TableName: USERS_ANSWERS_TABLE,
+        Item: {
+            userId: userId,
+            questionId: questionId,
+            answer: answer
+        },
+    };
+
+    dynamoDb.put(params, (error) => {
+        if (error) {
+            console.log(error);
+            res.status(400).json({ error: 'Could not create user' });
+        }
+        res.json({ userId, questionId, answer });
+    });
+})
+
 
 module.exports.handler = serverless(app);
