03a3e3041072b6f821e8ef40592a99edbd536fe3
royborer <royb@tikalk.com>
2018-02-20 13:22:30 +0200

    Added put user answer in a different file

--- putAnswer.js
+++ putAnswer.js
@@ -1,6 +1,33 @@
+const USERS_ANSWERS_TABLE = process.env.USER_ANSWERS_TABLE;
 
-function putUserAnswer(userId, questionId, answer) {
-    return "dummy";
+
+function putUserAnswer(dynamoDb, req, res) {
+   const { userId, questionId, answer } = req.body;
+        if (typeof userId !== 'string') {
+            res.status(400).json({ error: '"userId" must be a string' });
+        } else if (typeof questionId !== 'string') {
+            res.status(400).json({ error: '"name" must be a string' });
+        }
+        else if (typeof answer !== 'string') {
+            res.status(400).json({ error: '"name" must be a string' });
+        }
+
+        const params = {
+            TableName: USERS_ANSWERS_TABLE,
+            Item: {
+                userId: userId,
+                questionId: questionId,
+                answer: answer
+            },
+        };
+
+        dynamoDb.put(params, (error) => {
+            if (error) {
+                console.log(error);
+                res.status(400).json({ error: 'Could not create user' });
+            }
+            res.json({ userId, questionId, answer });
+        });
 }
 
 module.exports = {
