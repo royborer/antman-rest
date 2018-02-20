b20109fde2a4700670b4a70e7fe26d696cdd8c0c
evyatark <evyatar@tikalk.com>
2018-02-20 15:54:29 +0200

    change quiz

--- getOneQuestion.js
+++ getOneQuestion.js
@@ -31,24 +31,25 @@ function getQuiz(quizId) {
             isLast: false
         },
 
+        
         {  
             order: 2,
-            questionId : "124",
-            question : "Do you use Java?",
+            questionId : "126",
+            question : "What do you think of Avichay?",
             possibleAnswers : [ 
-                                { id:"1", text:"yes"},
-                                { id:"2", text:"no"}
+                                { id:"1", text:"sucks"},
+                                { id:"2", text:"great"}
                             ],
             isLast: false
         },
 
         {  
             order: 3,
-            questionId : "126",
-            question : "What do you think of Javascript?",
+            questionId : "124",
+            question : "Do you use Java?",
             possibleAnswers : [ 
-                                { id:"1", text:"sucks"},
-                                { id:"2", text:"great"}
+                                { id:"1", text:"yes"},
+                                { id:"2", text:"no"}
                             ],
             isLast: false
         },
